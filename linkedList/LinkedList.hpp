7bdeb2364a97f0cd9ff00ee2b2cc5b2576237ebc
Malena Schoeni <msch179@ku.edu>
2019-09-05 20:56:44 -0500

    Bug fix for search, score of tests now 100

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -55,8 +55,14 @@ bool LinkedList<T>::search(T value) const
 		if (temp->getValue() == value)
 		{
 			isFound = true;
+			return isFound;
 		}
 		temp = temp->getNext();
+	}
+	if (temp->getValue() == value)
+	{
+		isFound = true;
+		return isFound;
 	}
 
 	return(isFound);
